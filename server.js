89bcc0dc146d66940c2ba1cb5c7c09d92bb45638
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-02-07 10:07:37 +0530

    Updated CRUD routes and modified backend structure

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON
